17464d46a0470a5310349048e432b1792ef79aa6
sathwik-r <sathwikbollu@gmail.com>
2021-12-06 21:34:06 +0530

    added model

--- apis.py
+++ apis.py
@@ -4,8 +4,11 @@ import h5py
 from tensorflow import keras
 import numpy as np
 import pickle
+import os
+# import file_name.h5
+from keras.preprocessing import image
 
-classes={'Bacterial leaf blight': 0, 'Brown spot': 1, 'Leaf smut': 2}
+classes = {'Bacterial leaf blight': 0, 'Brown spot': 1, 'Leaf smut': 2}
 
 # s3_client=boto3.client('s3')
 # def upload_np_to_s3(image_np,transaction_id=None):
@@ -22,14 +25,23 @@ classes={'Bacterial leaf blight': 0, 'Brown spot': 1, 'Leaf smut': 2}
 #     # res=pickle.load(res)
 #     # print(res)
 
+
 def predict_image_class(image_np):
-    model=keras.models.load_model("file_name.h5")
-    model.summary()
-    prediction=model.predict(image_np)
+    model = keras.models.load_model("file_name.h5")
+    #model.summary()
+    preds = model.predict(image_np)
+    preds = np.argmax(preds)
+    print(preds)  # print the label of the class with maximum score
     for a in classes:
-        if(classes[a]==prediction):
+        if (classes[a] == preds):
             return a
     return ""
 
+
 # image_np=np.random.randn(10)
-# upload_np_to_s3(image_np=image_np)
+# img_width, img_height = 224, 224
+# img = image.load_img('index.jpeg', target_size = (img_width, img_height))
+# img = image.img_to_array(img)
+# img = np.expand_dims(img, axis = 0)
+# prediction=predict_image_class(img)
+# print("final",prediction)
--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,Response
+from flask import Flask, request, Response, json
 from tensorflow import keras
 from keras.preprocessing import image
 from io import BytesIO
@@ -6,31 +6,40 @@ from PIL import Image
 import jsonpickle
 import numpy as np
 from apis import predict_image_class
-application=Flask(__name__)
 
-@application.route('/',methods=['POST','GET'])
+application = Flask(__name__)
+
+
+@application.route('/', methods=['POST', 'GET'])
 def home_page():
     return 'Welcome To Plant Doctor Server'
+
+
 @application.route('/process_image', methods=['POST', 'GET'])
 def process_image():
     print("started")
-    image_path=request.files['image']
-    
-    if(image_path.filename!=''):
+    image_path = request.files['image']
+
+    if (image_path.filename != ''):
+        img_width, img_height = 224, 224
+        img = Image.open(image_path)
+        img = img.resize((224, 224))
         #img=misc.imread(image_path)
-        image_nparr=image.img_to_array(Image.open(image_path))
-        image_nparr = np.expand_dims(image_nparr, axis = 0)
+        #image_nparr=image.img_to_array(Image.open())
+        image_nparr = image.img_to_array(img)
+        image_nparr = np.expand_dims(image_nparr, axis=0)
         #image_path.save(image_path.filename)
     print(image_nparr)
-    prediction=predict_image_class(image_nparr)
-    print("prediction is ",prediction)
+    prediction = predict_image_class(image_nparr)
+    print("prediction is ", prediction)
     print(type(image_nparr))
-    res={
-        'plant_name':'b',
-        'disease_detected':prediction,
-        'more_info':"NA"
+    res = {
+        'plant_name': 'Rice',
+        'disease_detected': prediction,
+        'more_info': "NA"
     }
-    return Response(res, status=200, mimetype='application/json')
+    return Response(json.dumps(res), status=200, mimetype='application/json')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     application.run(debug=True)
